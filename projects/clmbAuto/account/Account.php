12911900e0b8c16d1bca1ac5bc9893b9600d3ca1
Omalka Nidarshana <omalkanidarshana@gmail.com>
2020-07-05 23:44:28 +0530

    add comment

--- projects/clmbAuto/account/Account.php
+++ projects/clmbAuto/account/Account.php
@@ -76,7 +76,7 @@ class Account{
     }
 
     function getUserInformation(){
-        $userData = geUserInformationByCmpId($this->link,$this->cmpId);
+        $userData = geUserInformationByCmpId($this->link,$this->cmpId);//get User Data By companyId;
         
         $html ='';
         $html .= '<div class="box-body table-responsive no-padding">';
